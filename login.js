9c7987470769dcd4c786dac8727644cb97281808
bysimeon <Simeon@dhcp-199-74-100-218.res-hall.northwestern.edu>
2017-02-10 03:14:00 -0600

    new material
    haha the puns

---
+++ login.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#loginform").on('keydown', 'input', function (event) {
+      if (event.which == 13) {
+          event.preventDefault();
+          var $this = $(event.target);
+          var index = parseFloat($this.attr('data-index'));
+          $('[data-index="' + (index + 1).toString() + '"]').focus();
+      }
+  });
+});
