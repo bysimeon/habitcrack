747229a7a20efba85ec5cc19398b95d0bd8d2879
Durellg <durellgill@gmail.com>
2017-02-20 18:46:53 -0600

    durelldagoat
    special delivery from the #durelldagoat

---
+++ settings.js
@@ -0,0 +1,8 @@
+function showDaysMessage (value) {
+  document.getElementById("numDays").innerHTML = value + ' Days';
+}
+
+  function showHoursMessage (value) {
+    document.getElementById("numHours").innerHTML = value;
+
+}
