00d0604164def3c49012b76218d17fa96c5b7a34
simeon <simeon@u.northwestern.edu>
2017-02-25 14:06:43 -0600

    storedData
    updated the stored data a bit for multiple habits. can't get the javascript for choosing which divs to show on the homepage on load still.

--- habits.js
+++ habits.js
@@ -1,6 +1,20 @@
 function selectHabit (value) {
   document.getElementById("habname").innerHTML = value;
-  localStorage.setItem("habit", value);
+  var habitnumber = localStorage.getItem("sethabits");
+  if (habitnumber == 0) {
+    localStorage.setItem("habit1", value);
+  }
+  if (habitnumber == 1) {
+    localStorage.setItem("habit2", value);
+  }
+  if (habitnumber == 2) {
+    localStorage.setItem("habit3", value);
+  }
+}
+
+function message1 () {
+  value = document.getElementById('remessage').innerHTML;
+  localStorage.setItem("message", value);
 }
 
 function showDaysMessage (value) {
@@ -9,5 +23,20 @@ function showDaysMessage (value) {
 
   function showHoursMessage (value) {
     document.getElementById("numHours").innerHTML = value;
+}
 
+function submit () {
+  location.href = 'home.html';
+  var habitnumber = localStorage.getItem("sethabits");
+  if (habitnumber == 0) {
+    localStorage.setItem("sethabits", 1);
+  }
+   if (habitnumber == 1) {
+    localStorage.setItem("sethabits", 2);
+  }
+  if (habitnumber == 2) {
+    localStorage.setItem("sethabits", 3);
+  } else {
+    
+  }
 }
