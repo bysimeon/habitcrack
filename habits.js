1d54caa8ceda32cfce11864162fab13f41ea9915
bysimeon <Simeon@dhcp-199-74-100-218.res-hall.northwestern.edu>
2017-02-25 17:49:55 -0600

    ///
    beautify

--- habits.js
+++ habits.js
@@ -1,42 +1,42 @@
-function selectHabit (value) {
-  document.getElementById("habname").innerHTML = value;
-  var habitnumber = localStorage.getItem("sethabits");
-  if (habitnumber == 0) {
-    localStorage.setItem("habit1", value);
-  }
-  if (habitnumber == 1) {
-    localStorage.setItem("habit2", value);
-  }
-  if (habitnumber == 2) {
-    localStorage.setItem("habit3", value);
-  }
+function selectHabit(value) {
+    document.getElementById("habname").innerHTML = value;
+    var habitnumber = localStorage.getItem("sethabits");
+    if (habitnumber == 0) {
+        localStorage.setItem("habit1", value);
+    }
+    if (habitnumber == 1) {
+        localStorage.setItem("habit2", value);
+    }
+    if (habitnumber == 2) {
+        localStorage.setItem("habit3", value);
+    }
 }
 
-function message1 () {
-  value = document.getElementById('remessage').innerHTML;
-  localStorage.setItem("message", value);
+function message1() {
+    value = document.getElementById('remessage').innerHTML;
+    localStorage.setItem("message", value);
 }
 
-function showDaysMessage (value) {
-  document.getElementById("numDays").innerHTML = value + ' Days';
+function showDaysMessage(value) {
+    document.getElementById("numDays").innerHTML = value + ' Days';
 }
 
-  function showHoursMessage (value) {
+function showHoursMessage(value) {
     document.getElementById("numHours").innerHTML = value;
 }
 
-function submit () {
-  location.href = 'home.html';
-  var habitnumber = localStorage.getItem("sethabits");
-  if (habitnumber == 0) {
-    localStorage.setItem("sethabits", 1);
-  }
-   if (habitnumber == 1) {
-    localStorage.setItem("sethabits", 2);
-  }
-  if (habitnumber == 2) {
-    localStorage.setItem("sethabits", 3);
-  } else {
-    
-  }
+function submit() {
+    location.href = 'home.html';
+    var habitnumber = localStorage.getItem("sethabits");
+    if (habitnumber == 0) {
+        localStorage.setItem("sethabits", 1);
+    }
+    if (habitnumber == 1) {
+        localStorage.setItem("sethabits", 2);
+    }
+    if (habitnumber == 2) {
+        localStorage.setItem("sethabits", 3);
+    } else {
+
+    }
 }
--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-function showDaysMessage (value) {
-  document.getElementById("numDays").innerHTML = value + ' Days';
+function showDaysMessage(value) {
+    document.getElementById("numDays").innerHTML = value + ' Days';
 }
 
-  function showHoursMessage (value) {
+function showHoursMessage(value) {
     document.getElementById("numHours").innerHTML = value;
 
 }
